02f1fb26298612cc8b13139a48ab4eb746438832
Brian Warner <bwarner@tffosx.ifn.private>
2014-08-06 13:57:07 -0700

    removed some prints

--- exercise_1.py
+++ exercise_1.py
@@ -37,19 +37,16 @@ def russian_alg(num1, num2):
         else:
             counter_num1.append(int(num1))
     c = len(counter_num1)
-    print(c)
     while c > 0:
         num2 = num2 * 2
         counter_num2.append(num2)
         c -= 1
-    print(counter_num2)
     for i in counter_num1:
         if int(i) % 2 == 1:
-            print(int(i))
             cn1 = counter_num1.index(i)
             sum_list.append(counter_num2[cn1])
     return sum(sum_list)
-russian_alg(5678,654)
+print(russian_alg(24,16))
 
 '''
 INSTRUCTORS CODE IS BELOW:  The Udemy course instructor had a much better method of achieving the same result.
